8659c07f81eb8b72d26ac96667b225475f2450be
Adrian Earl <adrianearl16@gmail.com>
2026-02-15 01:03:28 +0800

    picture placement

--- app/components/homepage/calendly.section.tsx
+++ app/components/homepage/calendly.section.tsx
@@ -87,7 +87,7 @@ export default function CalendlySection() {
             className="absolute hidden md:block left-0 lg:left-8 xl:left-12 top-1/2 -translate-y-1/2 w-[280px] h-[420px] shadow-2xl rounded-xl overflow-hidden z-0 border border-gray-200/50 opacity-60 hover:opacity-100 transition-opacity"
           >
             <Image
-              src="/images/suki2.jpg"
+              src="/images/starmodel/trulin.jpg"
               alt="Decoration"
               fill
               sizes="280px"
